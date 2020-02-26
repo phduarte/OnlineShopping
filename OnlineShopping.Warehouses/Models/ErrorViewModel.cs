30bd65df91fff2211b789cd7832be5b89175b707
Paulo Duarte <phduarte87@outlook.com>
2020-02-25 21:02:39 -0300

    Inclusão dos projetos de Web Client e configuração da solution para startar com múltiplos projetos.

---
+++ OnlineShopping.Warehouses/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnlineShopping.Warehouses.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
